156bbf4732ed1446fd05010fb1840e3c3866d395
oooo <149586090+rootoooo@users.noreply.github.com>
2024-06-10 22:56:40 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,6 +1,4 @@
 const routes = [
-  {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
   {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
-  {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
 ]
