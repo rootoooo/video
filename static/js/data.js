eac4b20bca566a4a4a03caf347d853daa839fa6b
oooo <149586090+rootoooo@users.noreply.github.com>
2024-07-20 02:38:34 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -4,7 +4,3 @@ const routes = [
   {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
   {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
 ]
-
-const friends = [
-  {name: 'Here200\'s Bookmark', url: 'https://here200.github.io/Bookmark/'},
-]
